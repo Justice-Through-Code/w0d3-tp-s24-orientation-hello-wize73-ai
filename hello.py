bc51336f8688f32eb5807816fe35ce1a44976556
asha maurya <ashamaurya@ashatekiMacBook-Pro.local>
2024-04-20 15:56:55 -0400

    Create W0D3 assignment

---
+++ hello.py
@@ -0,0 +1,9 @@
+def hello_world():
+    # Write your code here to print "Hello World!"
+    # Delete the 'pass' below
+    pass
+   
+
+# Do not modify the code below this line
+if __name__ == "__main__":
+    hello_world()
